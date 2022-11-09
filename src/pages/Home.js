f5254ada3303609e34b7f871290f61423106c97c
shanthanb <shanthanboppudi@gmail.com>
2022-11-09 22:03:44 +0530

    initial commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,18 @@
-import React, { lazy, Suspense } from 'react'
-import Navbar from 'components/Navbar';
-import MainContent from 'components/MainContent';
-// const LazyComponent = React.lazy(() => import('../components/Navbar'));
-// const LazyComponent1 = React.lazy(() => import('../components/MainContent'));
+import React, { Suspense } from 'react'
+// import Navbar from 'components/Navbar';
+// import MainContent from 'components/MainContent';
+const LazyComponent = React.lazy(() => import('../components/Navbar'));
+const LazyComponent1 = React.lazy(() => import('../components/MainContent'));
 const home = () => (
   (
     <div>
-      {/* //   <Suspense fallback={<div>Loading...</div>}>
-  //     {/* <LazyComponent />
-  //     <LazyComponent1 /> */}
-      {/* //   </Suspense> */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <LazyComponent />
+        <LazyComponent1 />
+      </Suspense>
 
-      <Navbar />
-      <MainContent />
+      {/* <Navbar />
+      <MainContent /> */}
     </div>)
 )
 export default home;
